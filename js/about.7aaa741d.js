dd26be66612e235f8ac6a4bf1940d02b10ea2b8b
Ricardo Pestana <oricardopestana@gmail.com>
2019-09-29 15:45:28 +0100

    Test for website push

---
+++ js/about.7aaa741d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("Made by "),n("span",{staticStyle:{color:"#0000FF"}},[t._v("underivative")])])])}],c=n("2877"),l={},r=Object(c["a"])(l,a,s,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=about.7aaa741d.js.map
